8522af8867d3a3d0e92b0818a8d02e69162bffc6
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-05-16 21:43:11 -0400

    added new method to render a line with DDA algorithm, added a new method to render a triangle that build upon the line method

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # graphics-engine
+
+To run this program make fure you have "make" library.
+
+- Once you have make in your linux/macOs or mingw env. type "make"
+- Look at the Makefile for more reference on which command does what
--- src/main.c
+++ src/main.c
@@ -98,10 +98,21 @@ void render(void) {
   // loop all projected points and render them
   for (int i = 0; i < N_MESH_FACES; i++) {
     triangle_t triangle = triangles_to_render[i];
+
+    // Draw vertex points
     draw_rectangle(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
     draw_rectangle(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
     draw_rectangle(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFFFF00);
+
+    // Draw unfilled triangle
+    draw_triangle(
+      triangle.points[0].x, triangle.points[0].y, 
+      triangle.points[1].x, triangle.points[1].y, 
+      triangle.points[2].x, triangle.points[2].y,
+      0xFF00F0F0
+    );
   }
+
   render_color_buffer();
   clear_color_buffer(0xFF000000);
   SDL_RenderPresent(renderer); 
